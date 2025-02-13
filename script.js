1e6df62e07eea53c54687e3d1521c1e096d1e339
AMINE-RPA <payet.rayyan@gmail.com>
2025-02-13 15:33:35 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+
+    const binId = "67ac3265acd3cb34a8deaac1"; // Remplace par ton ID JSONBin
+    const apiKey = "$2a$10$lsQYBX8I2jpoXqzfu0MI5uqMnOMUbgkL/i0wE7Gj7wUw5nAE1KqYe"; // Remplace par ton API Key JSONBin
