32b137ce8b8679eb5a541c90c63116565cc722e9
AMINE-RPA <payet.rayyan@gmail.com>
2025-02-13 15:57:32 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,8 @@
 // Variables JSONBin
 const binId = "67ac3265acd3cb34a8deaac1"; // Remplace par ton ID JSONBin
 const apiKey = "$2a$10$lsQYBX8I2jpoXqzfu0MI5uqMnOMUbgkL/i0wE7Gj7wUw5nAE1KqYe"; // Remplace par ton API Key JSONBin
-
+const JSON_BIN_ID = "67ac3265acd3cb34a8deaac1";
+const JSON_BIN_KEY = "$2a$10$lsQYBX8I2jpoXqzfu0MI5uqMnOMUbgkL/i0wE7Gj7wUw5nAE1KqYe";
 const JSON_BIN_URL = `https://api.jsonbin.io/v3/b/${JSON_BIN_ID}`;
 
 // Sélection des éléments HTML
